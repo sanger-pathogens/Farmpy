7517ecabb2f726fd8c79f1bb5f447af952a7cb40
Martin Hunt <mh12@sanger.ac.uk>
2017-01-09 16:51:47 +0000

    Remove long description (breaks pip3 with python 3.6)

--- setup.py
+++ setup.py
@@ -2,14 +2,10 @@ import os
 import glob
 from setuptools import setup, find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name='farmpy',
     version='0.3.1',
     description='Package to run farm jobs - currently LSF supported',
-    long_description=read('README.md'),
     author='Martin Hunt',
     author_email='mh12@sanger.ac.uk',
     url='https://github.com/martinghunt/farmpy',
